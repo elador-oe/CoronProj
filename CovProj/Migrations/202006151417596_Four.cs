c5535d4e0c9234a2d05bf31ed717b23b975903ed
elador <elador.oe@cs.colman.ac.il>
2020-06-15 17:19:11 +0300

    People create adding css

---
+++ CovProj/Migrations/202006151417596_Four.cs
@@ -0,0 +1,18 @@
+﻿namespace CovProj.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Four : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Sicks", "PlaceId", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Sicks", "PlaceId");
+        }
+    }
+}
