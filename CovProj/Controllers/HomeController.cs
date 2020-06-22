fdc445e5992691b7e491334b92e09c0cb6a03857
elador <elador.oe@cs.colman.ac.il>
2020-06-22 19:17:19 +0300

    few changes

--- CovProj/Controllers/HomeController.cs
+++ CovProj/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CovProj.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace CovProj.Controllers
 {
     public class HomeController : Controller
     {
+        private CovidDBContext db = new CovidDBContext();
         public ActionResult Index()
         {
             return View();
@@ -17,11 +19,33 @@ namespace CovProj.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Login(Peoples people)
         {
-            ViewBag.Message = "Your application description page.";
+            var loggedUser = db.peoples.Where(u => u.Email == people.Email).FirstOrDefault();
+
+            if (loggedUser != null)
+            {
+                SetSessionForUser(loggedUser);
+                return RedirectToAction("Index");
+            }
+
+            else
+                ModelState.AddModelError("", "Wrong credentials");
 
             return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+        private bool EmailAlreadyTaken(string email)
+        {
+            Peoples user = db.peoples.AsNoTracking().Where(u => u.Email == email).FirstOrDefault();
+
+            return (user != null);
         }
 
         public ActionResult Contact()
@@ -29,6 +53,23 @@ namespace CovProj.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult AccessDenied(string ErrorMessage)
+        {
+            if (ErrorMessage != null)
+            {
+                ViewBag.ErrorMessage = ErrorMessage;
+            }
+            return View();
+        }
+        private void SetSessionForUser(Peoples loggedUser)
+        {
+            Session["UserID"] = loggedUser.PeoplesId.ToString();
+            Session["Username"] = loggedUser.Email.ToString();
+            Session["Fullname"] = loggedUser.FirstName.ToString() + " " + loggedUser.LastName.ToString();
+
+            if (loggedUser.IsAdmin)
+                Session["Admin"] = "true";
         }
 
         public ActionResult Login()
